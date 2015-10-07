4068b9e4bec82052f397a6ed7aebef83b72573b9
jaybach <dr.jaybach@gmail.com>
2015-10-07 22:09:27 +0000

    adding and listing contacts properly working

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -2,11 +2,13 @@
 
 // });
 
-
+// alert("submitted form");
 $(function() {
   var newContact = $('#add-contact');
   var contactsList = $('.contacts');
-  $newContact.on('submit', function() {
+
+  newContact.on('submit', function() {
+
     $.ajax({
 
       // going to make a call to post to your create route
@@ -14,21 +16,28 @@ $(function() {
       // look up serialize method
       // ajax is either success or failure; if success, it returns json
       // use that json to append to the html
+
+      // action and method below refer to the html form stuff from index.erb:  <form id="add-contact" action="/" method="post">
       url: newContact.attr('action'),
       method: newContact.attr('method'),
       data: newContact.serialize(),
       dataType: 'json',
       success: function(contact) {
+        // the below empties out the form inputs when the form is submitted
         newContact[0].reset();
         contactsList.append(getContactHTML(contact));
       }
 
     });
 
-    function getContactHTML(contact) {
-      return '<div class="contact"><h4>' + contact.name + '</h4><h5>' + contact.email '</h5>';
-    }
+    // returning false keeps the page from reloading
+    return false;
 
   });
 
+    function getContactHTML(contact) {
+      return '<div class="contact"><h4>' + contact.name + '</h4><h5>' + contact.email + '</h5></div><hr>';
+    }
+
 });
+
